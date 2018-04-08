93cce35d4d843fc97314e9ae19d0f1d16865b3d4
Daniel <github@profoundideation.com>
2018-04-08 05:15:31 -0400

    part 1

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ let Book = require('./book');
 
 mongoose.connect('mongodb://localhost/mongoose_basics', function (err) {
     if (err) throw err;
+    
     console.log('Successfully Connected');
+
+    let jamieAuthor = new Author({
+        _id: new mongoose.Types.ObjectId(),
+        name: {
+            firstName: 'Jamie',
+            lastName: 'Munro'
+        },
+        biography: 'Jamie is the author of ASP.NET MVC 5 with Bootstrap and Knockout.js.',
+        twitter: 'https://twitter.com/endyourif',
+        facebook: 'https://www.facebook.com/End-Your-If-194251957252562/'
+    });
+
 });
