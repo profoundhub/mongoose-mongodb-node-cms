e9d869af4324c02df9e47d718fe19ed6be63606b
Daniel <github@profoundideation.com>
2018-04-08 05:19:13 -0400

    part 2

--- index.js
+++ index.js
@@ -19,4 +19,37 @@ mongoose.connect('mongodb://localhost/mongoose_basics', function (err) {
         facebook: 'https://www.facebook.com/End-Your-If-194251957252562/'
     });
 
+    jamieAuthor.save(function(err) {
+        if (err) throw err;
+         
+        console.log('Author successfully saved.');
+         
+        var mvcBook = new Book({
+            _id: new mongoose.Types.ObjectId(),
+            title: 'ASP.NET MVC 5 with Bootstrap and Knockout.js',
+            author: jamieAuthor._id,
+            ratings:[{
+                summary: 'Great read'
+            }]
+        });
+         
+        mvcBook.save(function(err) {
+            if (err) throw err;
+         
+            console.log('Book successfully saved.');
+        });
+         
+        var knockoutBook = new Book({
+            _id: new mongoose.Types.ObjectId(),
+            title: 'Knockout.js: Building Dynamic Client-Side Web Applications',
+            author: jamieAuthor._id
+        });
+         
+        knockoutBook.save(function(err) {
+            if (err) throw err;
+         
+            console.log('Book successfully saved.');
+        });
+    });
+    
 });
